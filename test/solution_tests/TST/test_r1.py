647c5b88c4ee8f90997b682a300632da64ea3b17
sbx_user1051 <sbx_user1051@169.254.53.129>
2024-04-23 14:07:17 +0000

    Tue Apr 23 14:07:17 UTC 2024

---
+++ test/solution_tests/TST/test_r1.py
@@ -0,0 +1,16 @@
+from solutions.CHK import checkout
+
+class TestCheckout():
+    def test_invalid(self):
+        assert checkout([]) == -1
+        assert checkout(9) == -1
+
+    def test_empty(self):
+        assert checkout("") == 0
+
+    def test_non_sale(self):
+        assert checkout("ddd") == 45
+        assert checkout("cd") == 35
+
+    def test_sale(self):
+        assert checkout("aaa") == 130
