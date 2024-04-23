76616b2b720eb8a08a210a20b5df0b5a0bc22904
sbx_user1051 <sbx_user1051@169.254.53.129>
2024-04-23 14:01:17 +0000

    Tue Apr 23 14:01:17 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,29 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # define prices and offers
+    prices = {"a": 50, "b": 30, "c": 20, "d": 15}
+    x_for_y = {"a": {"units": 3, "price": 130}, "b": {"units": 2, "price": 45}}
+
+    # handle offer logic
+    def x_for_y_calculator(actual_units: int, offer_units: int, offer_price: int, regular_price: int):
+        subtotal = 0
+        while actual_units > offer_units:
+            actual_units - offer_units
+            subtotal += offer_price
+        subtotal += (actual_units * regular_price)
+
+    # init result price
+    total = 0
+    deals = {"a": 0, "b": 0}
+
+    # deal with stream of skus
+    for sku in skus:
+        if sku in x_for_y.keys():
+            deals[sku] += 1
+        else:
+            total += sku * prices[sku]
+    for sku in deals.keys():
+        total += x_for_y_calculator(deals[sku], x_for_y[sku].units, x_for_y[sku].price, prices[sku])
+    
+    return total
